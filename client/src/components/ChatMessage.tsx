4d91aef20d03475ddb1d91706e193d496c93c11d
humandavey <davey.adams.three@gmail.com>
2025-01-15 19:56:55 -0600

    feat: add discussion ui

---
+++ client/src/components/ChatMessage.tsx
@@ -0,0 +1,36 @@
+interface ChatMessageProps {
+  name: string,
+  timestamp: number,
+  content: string,
+}
+
+export default ({name, timestamp, content}: ChatMessageProps) => {
+
+  const formatTime = (timestamp: number): string => {
+    const seconds = (Date.now() / 1000) - timestamp;
+    const minutes = seconds / 60;
+    const hours = minutes / 60;
+    const days = hours / 60;
+    if (days >= 1) return Math.floor(days) + 'd ago';
+    if (hours >= 1) return Math.floor(hours) + 'h ago';
+    if (minutes >= 1) return Math.floor(minutes) + 'm ago';
+    return Math.floor(seconds) + 's ago';
+  }
+
+  return (
+    <>
+      <div className='chat-message'>
+        <img className='chat-profile-pic' src={'https://api.dicebear.com/6.x/initials/svg?seed=' + name} />
+        <div className='chat-message-content'>
+          <div className='chat-message-info'>
+            <p style={{margin: 0}}>{name}</p>
+            <p style={{margin: 0, color: '#6b7280'}}>{formatTime(timestamp)}</p>
+          </div>
+          <div>
+            <p style={{margin: 0, fontSize: 14, color: '#374151'}}>{content}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import ChatMessage from "../components/ChatMessage";
 import Header from "../components/Header";
 import LeagueSelector from "../components/LeagueSelector";
 import ScorePreview from "../components/ScorePreview";
@@ -9,7 +10,7 @@ export default () => {
       <div className='content'>
         <Header />
         <div className='home-content'>
-          <div className='live-scores-container'>
+          <div className='card-container'>
             <div>
               <h2 style={{margin: 0}}>Live Scores</h2>
             </div>
@@ -18,6 +19,18 @@ export default () => {
             </div>
             <div className='scores-container'>
               <ScorePreview home='Celtics' homeScore={88} moment='Q3 5:45' away='Nets' awayScore={85}/>
+            </div>
+          </div>
+          <div className='card-container discussion-container'>
+            <div>
+              <h2 style={{margin: 0}}>Discussion</h2>
+              <div className='chat-container'>
+                <ChatMessage name='John Doe' timestamp={0} content={"What a game! The Lakers are really struggling in the 4th quarter!"} />
+              </div>
+            </div>
+            <div className='chat-send-message'>
+              <input className='chat-input' placeholder='Add to the discussion' type='text' />
+              <button className='chat-post'>Post Comment</button>
             </div>
           </div>
         </div>
