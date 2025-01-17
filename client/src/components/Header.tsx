1924f110068e8e980ac51678e3fce99f50cbb22f
humandavey <davey.adams.three@gmail.com>
2025-01-17 12:33:03 -0600

    fix: make nav turn to dropdown for smaller screens and less margin

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,20 +1,53 @@
+import { useLayoutEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+const useWidth = () => {
+  const [width, setWidth] = useState(0);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return width;
+}
 
 export default () => {
 
   const navigate = useNavigate();
+  const width = useWidth();
 
   return (
     <>
       <div className='header-container'>
         <img tabIndex={0} className='header-img' src='./logo_black.svg' />
-        <div className='header-nav'>
-          <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/')}>Home</p>
-          <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/scores')}>Scores</p>
-          <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/schedule')}>Schedule</p>
-          <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/discussion')}>Discussion</p>
-          <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/account')}>Account</p>
-        </div>
+        {
+          width > 528 ?
+          <>
+            <div className='header-nav'>
+              <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/')}>Home</p>
+              <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/scores')}>Scores</p>
+              <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/schedule')}>Schedule</p>
+              <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/discussion')}>Discussion</p>
+              <p tabIndex={0} className='header-nav-item' onClick={() => navigate('/account')}>Account</p>
+            </div>
+          </>
+          :
+          <>
+            <div className="header-nav">
+              <select className='small-nav' name='navigation' onChange={(e) => navigate('/' + e.target.value)} defaultValue={'Navigation'}>
+                <option value='Navigation' disabled hidden>Navigation</option>
+                <option value=''>Home</option>
+                <option value='scores'>Scores</option>
+                <option value='schedule'>Schedule</option>
+                <option value='discussion'>Discussion</option>
+                <option value='account'>Account</option>
+              </select>
+            </div>
+          </>
+        }
       </div>
     </>
   );
